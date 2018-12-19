9b31a08e480b8acc724bd59722835fe7360f4bb2
Landrok <landrok@users.noreply.github.com>
2018-12-19 13:47:14 +0100

    Add toArray() method

--- tests/ActivityPub/Type/AbstractObjectTest.php
+++ tests/ActivityPub/Type/AbstractObjectTest.php
@@ -2,6 +2,7 @@
 
 namespace ActivityPubTest\Type;
 
+use ActivityPub\Type\Core\Link;
 use ActivityPub\Type\Core\ObjectType;
 use PHPUnit\Framework\TestCase;
 
@@ -25,7 +26,7 @@ class AbstractObjectTest extends TestCase
         $value = 'http://example3.com';
         $type->setId($value);
         $this->assertEquals($value, $type->id);
-	}
+    }
 
     /**
      * Test valid getters
@@ -45,38 +46,94 @@ class AbstractObjectTest extends TestCase
         $value = 'http://example3.com';
         $type->setId($value);
         $this->assertEquals($value, $type->get('id'));
-	}
+    }
 
-	/**
-	 * Getting a non defined should throw an exception
+    /**
+     * Getting a non defined should throw an exception
      * 
      * @expectedException \Exception
-	 */
-	public function testGetEmptyProperty()
-	{
-		$object = new ObjectType();
+     */
+    public function testGetEmptyProperty()
+    {
+        $object = new ObjectType();
         $object->myCustomAttribute;
-	}
+    }
 
-	/**
-	 * Setting without argument should throw an exception
+    /**
+     * Setting without argument should throw an exception
      * 
      * @expectedException \Exception
-	 */
-	public function testSetWithNoArgument()
-	{
-		$object = new ObjectType();
+     */
+    public function testSetWithNoArgument()
+    {
+        $object = new ObjectType();
         $object->setMyCustomAttribute();
-	}
+    }
 
-	/**
-	 * Call an undefined method
+    /**
+     * Call an undefined method
      * 
      * @expectedException \Exception
-	 */
-	public function testCallUndefinedMethod()
-	{
-		$object = new ObjectType();
+     */
+    public function testCallUndefinedMethod()
+    {
+        $object = new ObjectType();
         $object->illegalCall();
-	}
+    }
+
+    /**
+     * tests getProperties() method
+     */
+    public function testGetProperties()
+    {	
+        $object = new Link();
+
+        $expected = [
+            'type',
+            'id',
+            'name',
+            'nameMap',
+            'href',
+            'hreflang',
+            'mediaType',
+            'rel',
+            'height',
+            'preview',
+            'width'
+        ];
+
+        $this->assertEquals(
+            $expected, 
+            $object->getProperties()
+        );
+    }
+
+    /**
+     * tests toArray() method
+     */
+    public function testToArray()
+    {	
+        $link = new Link();
+        $link->setName('An example');
+        $link->setHref('http://example.com');
+
+        $expected = [
+            'type' => 'Link',
+            'id' => null,
+            'name' => 'An example',
+            'nameMap' =>null,
+            'href' => 'http://example.com',
+            'hreflang' =>null,
+            'mediaType' =>null,
+            'rel' =>null,
+            'height' =>null,
+            'preview' =>null,
+            'width' =>null,
+        ];
+
+        $this->assertEquals(
+            $expected, 
+            $link->toArray()
+        );
+    }
 }
