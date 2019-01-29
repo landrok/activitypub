802120985561f36f735df58035c766a07c6499fd
Landrok <landrok@users.noreply.github.com>
2019-01-29 14:40:05 +0100

    Fix HHVM tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 if (substr(php_uname(), 0, 7) !== 'Windows') {
-    
+
     $host = 'localhost';
     $port = 8000;
 
@@ -12,14 +12,14 @@ if (substr(php_uname(), 0, 7) !== 'Windows') {
         'php -S %s:%d  %s >/dev/null 2>&1 & echo $!',
         $host,
         $port,
-        dirname(__DIR__, 1) . '/tests/WebServer/router.php'
+        __DIR__ . '/WebServer/router.php'
     );
-     
+
     // Execute the command and store the process ID
     $output = array(); 
     exec($command, $output);
     $pid = (int) $output[0];
-     
+
     echo sprintf(
         '%s - Web server started on %s:%d with PID %d', 
         date('r'),
@@ -27,7 +27,7 @@ if (substr(php_uname(), 0, 7) !== 'Windows') {
         $port, 
         $pid
     ) . PHP_EOL;
-     
+
     // Kill the web server when the process ends
     register_shutdown_function(function() use ($pid) {
         echo sprintf('%s - Killing WebServer process with ID %d', date('r'), $pid) . PHP_EOL;
