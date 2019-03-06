214c145af4699e6b5acaf3fdd834dace57ab1272
dem <landrok@users.noreply.github.com>
2019-03-06 23:52:10 +0100

    Add scenarios for cache helper tests

--- tests/ActivityPhp/Server/CacheTest.php
+++ tests/ActivityPhp/Server/CacheTest.php
@@ -6,14 +6,14 @@ use ActivityPhp\Server;
 use PHPUnit\Framework\TestCase;
 
 /*
- * These scenarios are around server side actor instance
+ * These scenarios are around server caching
  */
-class ActorTest extends TestCase
+class CacheTest extends TestCase
 {
     /**
-     * Check that an actor has no public key
+     * Check that an actor is cached
      */
-    public function testActorWithNoPublicKeyPem()
+    public function testActorCacheSet()
     {
         $server = new Server([
             'instance' => [
@@ -26,13 +26,14 @@ class ActorTest extends TestCase
                'driver' => '\Psr\Log\NullLogger'
             ],
             'cache' => [
-                'enabled' => false,
+                'enabled' => true,
             ]
         ]);
 
         $actor = $server->actor('bob@localhost:8000');
+        $actor = $server->actor('bob@localhost:8000');
 
-        // Assert no public is set
+        // Assert no public key is set
         $this->assertEquals(
             false,
             $actor->getPublicKeyPem()
