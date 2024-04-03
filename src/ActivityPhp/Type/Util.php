610252e8698aed0aefd95cbcbfd72ffe9be8a49f
landrok <landrok@users.noreply.github.com>
2024-04-03 09:01:18 +0200

    [Feat] URL validator supports WebSocket Secure protocol

--- src/ActivityPhp/Type/Util.php
+++ src/ActivityPhp/Type/Util.php
@@ -62,7 +62,7 @@ abstract class Util
             && filter_var($value, FILTER_VALIDATE_URL) !== false
             && in_array(
                 parse_url($value, PHP_URL_SCHEME),
-                ['http', 'https', 'magnet']
+                ['http', 'https', 'magnet', 'wss']
             );
     }
 
